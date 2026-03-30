016519adfd8c1bf270ed8387f36434d9ff9fc1e4
LuizhBrandao <luizh.brandaoo@gmail.com>
2026-03-30 19:01:00 -0300

    feat: adiciona CampanhasDbContext para mapeamento PostgreSQL

---
+++ src/Fiap.OngEsperanca.Campanhas.Api/Infrastructure/Persistence/Relational/CampanhasDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fiap.OngEsperanca.Campanhas.Api.Infrastructure.Persistence.Relational
+{
+    internal class CampanhasDbContext
+    {
+    }
+}
