5911ee1a12be5231521c31009b89584e9e0f98f0
LuizhBrandao <luizh.brandaoo@gmail.com>
2026-03-30 22:36:24 -0300

    feat: adiciona classe Result, Repositorio e configura Program.cs

---
+++ src/Fiap.OngEsperanca.Campanhas.Api/Infrastructure/Persistence/Relational/Repositories/CampanhaRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Fiap.OngEsperanca.Campanhas.Api.Domain.Entities;
+using Fiap.OngEsperanca.Campanhas.Api.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiap.OngEsperanca.Campanhas.Api.Infrastructure.Persistence.Relational.Repositories;
+
+public class CampanhaRepository(CampanhasDbContext dbContext) : ICampanhaRepository
+{
+    public async Task<Campanha?> ObterPorIdAsync(Guid id, CancellationToken ct = default)
+    {
+        return await dbContext.Campanhas.FirstOrDefaultAsync(c => c.Id == id, ct);
+    }
+
+    public async Task AdicionarAsync(Campanha campanha, CancellationToken ct = default)
+    {
+        await dbContext.Campanhas.AddAsync(campanha, ct);
+    }
+
+    public async Task AtualizarAsync(Campanha campanha, CancellationToken ct = default)
+    {
+        dbContext.Campanhas.Update(campanha);
+        await Task.CompletedTask; // Update não é assíncrono no EF Core, mas mantemos a assinatura
+    }
+}
